51da510a9117535c0f3f169e9f9a48c92dd421bd
Eric Richard <reric0802@gmail.com>
2025-02-14 19:00:55 +0300

    added port

--- app.py
+++ app.py
@@ -191,4 +191,5 @@ def delete_note(note_id):
     db.session.commit()
     return jsonify({'message': 'Note deleted successfully'}), 200
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get port from environment, default to 5000
+    app.run(debug=False, host='0.0.0.0', port=port)
